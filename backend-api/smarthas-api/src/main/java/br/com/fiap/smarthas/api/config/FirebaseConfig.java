9f7e8ab2fa57b21e399b12d82428e547cecc9c62
Gustavo Pereira Marques <gustavoper.marq@gmail.com>
2026-08-28 21:54:08 -0300

    Arquivo novo Docker - Integração com render

--- backend-api/smarthas-api/src/main/java/br/com/fiap/smarthas/api/config/FirebaseConfig.java
+++ backend-api/smarthas-api/src/main/java/br/com/fiap/smarthas/api/config/FirebaseConfig.java
@@ -6,6 +6,7 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
 
 import jakarta.annotation.PostConstruct;
+import java.io.FileInputStream;
 import java.io.InputStream;
 
 @Configuration
@@ -14,18 +15,24 @@ public class FirebaseConfig {
     @PostConstruct
     public void init() {
         try {
+            InputStream serviceAccount;
+            String credenciaisPath = System.getenv("FIREBASE_CREDENTIALS_PATH");
 
-
-            InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("ServiceAccountKey.json");
+            if (credenciaisPath != null && !credenciaisPath.isBlank()) {
+                
+                serviceAccount = new FileInputStream(credenciaisPath);
+            } else {
+                
+                serviceAccount = getClass().getClassLoader().getResourceAsStream("ServiceAccountKey.json");
+            }
 
             if (serviceAccount == null) {
-                throw new RuntimeException("Arquivo ServiceAccountKey.json não encontrado no classpath.");
+                throw new RuntimeException("Credenciais do Firebase não encontradas.");
             }
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
-
 
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
